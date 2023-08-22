3307673d74321af4f252b8ee6d3dde6c5d1c4d9a
Nicole Moncrieffe <nicoletmoncrieffe@gmail.com>
2023-08-22 10:59:02 +0100

    api added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import './App.css';
 
 import Movies from './Movies.js';
 
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: [
+      'Montserrat:ital,wght@0,100;0,200;0,500;1,200;1,700',
+    ],
+  },
+});
+
+
 function App() {
   return (
     <div className="container-wrapper">
--- src/Movies.js
+++ src/Movies.js
@@ -1,23 +1,24 @@
 import { Card } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 import './Movies.css';
 
 function Movies(){
+  const apiKey = process.env.REACT_APP_API_OMDB_API_KEY;
 
   // User must type in valid movie title
   // Should return title, year, plot, base url and poster
   // to do url https://www.imdb.com/title/${url.imdbID}
 
   const [title, setTitle] = useState(''); // useState for movie title
-  const [year, setYear] = useState(''); // useState for year of release
-  const [plot, setPlot] = useState(''); // useState for movie plot
-  const [id, setId] = useState(''); // useState for ID to use on imdb url
-  const [poster, setPoster] = useState(''); // useState for movie poster
+  const [movies, setMovies] = useState([]); // useState empty array
+  const [formattedTitle, setFormattedTitle] = useState(''); // useState for storing the title input
 
-  const [searchResults, setSearchResults] = useState(false); // useState for search results p tag
+  const [formSubmitted, setFormSubmitted] = useState(false); // useState for submitting the form
+  const [resultsTitle, setResultsTitle] = useState(false); // useState for search results for ${movie}
+  const [searchError, setSearchError] = useState(false); // useState for search results p tag
 
 
   function handleChange(event){
@@ -25,21 +26,42 @@ function Movies(){
     const searchInput = event.target.value; // stores the movie title searched
     setTitle(searchInput); // defining the movie title
 
-    setSearchResults(false);
+    setSearchError(false);
+    setResultsTitle(false);
   };
 
 
   function handleSubmit(event){
     event.preventDefault();
     // console.log(event);
+    setFormSubmitted(true);
   };
 
 
+  useEffect(() => {
+    if (formSubmitted && title) {
+      console.log('Fetching data...');
+      fetch(`http://www.omdbapi.com/?s=${title}&apikey=${apiKey}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+        setMovies(data.Search);
+        setFormattedTitle(title);
 
+        setResultsTitle(true);
+        setFormSubmitted(false);
+        setSearchError(false);
+        setTitle(''); // clear input field after form submission
+      })
 
+      .catch((error) => {
+        console.error("Error fetching from omdb:", error);
+        setSearchError(true);
+        setResultsTitle(false);
+      })
 
-
-
+    }
+  }, [apiKey, title, formSubmitted]);
 
 
   return(
@@ -54,55 +76,38 @@ function Movies(){
       {/* SEARCHBAR HERE */}
       <div className='searchbar-wrapper'>
         <form onSubmit={handleSubmit} className='searchbar'>
-          <input onChange={handleChange} className='search-input' type='text' placeholder='Search CineQuest...'/>
+          <input onChange={handleChange} className='search-input' type='text' value={title} placeholder='Search CineQuest...'/>
           <input className='search-btn' type='submit'></input>
         </form>
       </div>
       </div>
 
       {/* MOVIE CARDS SECTION BELOW */}
-      <p className='search-results'>{searchResults === true ? '3' : 'No'} search results</p>
+      <p className={searchError === true || movies.length === 0 ? 'search-results' : 'search-results-hidden'}>No search results</p>
+      <h3 className={resultsTitle === true && movies.length > 0 ? 'results-title' : 'results-title-hidden'}>{movies.length} search results for <strong>{formattedTitle.charAt(0).toUpperCase() + formattedTitle.slice(1)}</strong></h3>
 
       <div className='movies-wrapper'>
-        {/* CARD 1  */}
-        <Card className='movie-card'>
-          <Card.Img className='movie-img' src='https://artofthemovies.co.uk/cdn/shop/products/IMG_2299-850371.jpg?v=1611688017' alt='movie-poster'/>
-          <Card.Body className='content'>
-            <Card.Text className='movie-title'>Harry Potter and the Goblet of Fire</Card.Text>
-            <Card.Text className='movie-year'>2005</Card.Text>
-            <Card.Text className='movie-plot'>Harry Potter finds himself competing in a hazardous tournament between rival schools of magic, but he is distracted by recurring nightmares.</Card.Text>
-            <a  href='#' target='_blank'><Button className='movie-btn'>View on imdb</Button></a>
-          </Card.Body>
-        </Card>
+        {movies && movies.length > 0 ? (
 
-        {/* CARD 2  */}
-        <Card className='movie-card'>
-          <Card.Img className='movie-img' src='https://artofthemovies.co.uk/cdn/shop/products/IMG_2299-850371.jpg?v=1611688017' alt='movie-poster'/>
-          <Card.Body className='content'>
-            <Card.Text className='movie-title'>Harry Potter and the Goblet of Fire</Card.Text>
-            <Card.Text className='movie-year'>2005</Card.Text>
-            <Card.Text className='movie-plot'>Harry Potter finds himself competing in a hazardous tournament between rival schools of magic, but he is distracted by recurring nightmares.</Card.Text>
-            <a  href='#' target='_blank'><Button className='movie-btn'>View on imdb</Button></a>
-          </Card.Body>
-        </Card>
+          movies.map((movie, index) => (
+            <Card className='movie-card'>
+              <Card.Img className='movie-img' src={movie.Poster !== 'N/A' ? movie.Poster : 'https://s3.amazonaws.com/shecodesio-production/uploads/files/000/094/199/original/fd2d9f4640394679d65967c13ec0de2c.jpg?1692633511'} alt='movie-poster'/>
+              <Card.Body className='content'>
+                <Card.Text className='movie-title'>{movie.Title}</Card.Text>
+                <Card.Text className='movie-year'>{movie.Year}</Card.Text>
+                <Card.Text className='movie-plot'>{movie.Plot}</Card.Text>
+                <a  href={`https://www.imdb.com/title/${movie.imdbID}`} target='_blank' rel="noreferrer"><Button className='movie-btn'>View on imdb</Button></a>
+              </Card.Body>
+            </Card>
 
-        {/* CARD 3  */}
-        <Card className='movie-card'>
-          <Card.Img className='movie-img' src='https://artofthemovies.co.uk/cdn/shop/products/IMG_2299-850371.jpg?v=1611688017' alt='movie-poster'/>
-          <Card.Body className='content'>
-            <Card.Text className='movie-title'>Harry Potter and the Goblet of Fire</Card.Text>
-            <Card.Text className='movie-year'>2005</Card.Text>
-            <Card.Text className='movie-plot'>Harry Potter finds himself competing in a hazardous tournament between rival schools of magic, but he is distracted by recurring nightmares.</Card.Text>
-            <a  href='#' target='_blank'><Button className='movie-btn'>View on imdb</Button></a>
-          </Card.Body>
-        </Card>
-
-
-
+          ))
+        ) : (
+          <></>
+        )}
       </div>
 
 
-
+      <footer>© Copyright Nicole Moncrieffe 2023</footer>
     </div>
   )
 
